c81db7c73b2893770568b410969652121b127392
SergWebAcceptance <serg.web@acceptance.ltd>
2025-05-12 13:58:13 +0300

    changed facebook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -168,7 +168,7 @@ const Header = () => {
             </Link>
           </div>
           <div className="soc">
-            <Link href="https://www.facebook.com/techfresco/">
+            <Link href="https://www.facebook.com/techfrescocom/">
               <Facebook />
             </Link>
             <Link href="https://www.instagram.com/techfresco/">
